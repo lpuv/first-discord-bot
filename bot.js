3adcda7083dcfe6c7f7e7d777988a433218901f0
Gamer4Life Puvilland <uyenwontanswer@gmail.com>
2019-09-17 16:55:17 +0000

    Added server

--- bot.js
+++ bot.js
@@ -1,15 +1,35 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.on('ready', () => {
+
+const http = require('http')
+const port = process.env.PORT
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', msg => {
+
+
+client.login(process.env.AUTH)
+
+  client.on('message', msg => {
   if (msg.content === '/ping') {
     msg.reply('Pong!')
     console.log("Ping command recieved")
   }
 })
 
-client.login(process.env.AUTH)
+  console.log(`server is listening on ${port}`)
+})
